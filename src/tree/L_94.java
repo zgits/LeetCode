5419addb25d669c17f32709ce242bc718a1a8519
zgits <43234936+zgits@users.noreply.github.com>
2019-03-20 22:08:21 +0800

    94

--- src/tree/L_94.java
+++ src/tree/L_94.java
@@ -2,6 +2,7 @@ package tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @Description: leetcode 94 二叉树的中序遍历
@@ -12,10 +13,30 @@ public class L_94 {
     public List<Integer> inorderTraversal(TreeNode root) {
 
         List<Integer> list=new ArrayList<>();
+        //解法1，递归，容易理解
+        // addToList(root,list);
+        Stack<TreeNode> stack=new Stack<>();
+        TreeNode cur=root;
+        while (cur!=null||!stack.isEmpty()){
+            if(cur!=null){
+                stack.push(cur);
+                cur=cur.left;
+            }else{
+                TreeNode treeNode=stack.pop();
+                list.add(treeNode.val);
+                cur=treeNode.right;
+            }
+        }
 
-        
-
-        return null;
+        return list;
+    }
+    public void addToList(TreeNode t,List<Integer> list){
+        if(t==null){
+            return ;
+        }
+        addToList(t.left,list);
+        list.add(t.val);
+        addToList(t.right,list);
     }
 }
 
