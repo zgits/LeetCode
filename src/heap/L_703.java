24dd3ecb6395ce313cb31afe7836f04f891445b5
zgits <2276479295@qq.com>
2019-04-08 17:40:46 +0800

    新增堆，完成了栈中的简单类别的题目

---
+++ src/heap/L_703.java
@@ -0,0 +1,23 @@
+package heap;
+
+/**
+ * @Author: zjf
+ * @Date: 2019/4/8 17:38
+ * @Description: 703. 数据流中的第K大元素
+ */
+public class L_703 {
+
+    /**
+     * 原题的类名为：KthLargest
+     * @param k
+     * @param nums
+     */
+    public L_703(int k, int[] nums) {
+
+    }
+
+    public int add(int val) {
+
+        return 0;
+    }
+}
